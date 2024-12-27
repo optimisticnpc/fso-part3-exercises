63a1c62462c31dbd154614fdfd7e541c5d43e457
optimisticnpc <joshuajltan@gmail.com>
2024-12-28 10:27:56 +1300

    Finished 3-3

--- index.js
+++ index.js
@@ -43,17 +43,16 @@ let persons = [
   })
 
 
-
-  // app.get('/api/notes/:id', (request, response) => {
-  //   const id = request.params.id
-  //   const note = notes.find(note => note.id === id)
+  app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
     
-  //   if (note) {
-  //     response.json(note)
-  //   } else {
-  //     response.status(404).end()
-  //   }
-  // })
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
 
   // const generateId = () => {
   //   const maxId = notes.length > 0
