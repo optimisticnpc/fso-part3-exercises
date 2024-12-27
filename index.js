6c01103fc5c030859c3e618fd482682135dbec8c
optimisticnpc <joshuajltan@gmail.com>
2024-12-28 10:37:52 +1300

    Finished 3-5

--- index.js
+++ index.js
@@ -61,32 +61,29 @@ let persons = [
     response.status(204).end()
   })
 
-  // const generateId = () => {
-  //   const maxId = notes.length > 0
-  //     ? Math.max(...notes.map(n => Number(n.id)))
-  //     : 0
-  //   return String(maxId + 1)
-  // }
+  const generateId = () => {
+    return String(Math.floor(Math.random() * 100000000))
+  }
   
-  // app.post('/api/notes', (request, response) => {
-  //   const body = request.body
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
   
-  //   if (!body.content) {
-  //     return response.status(400).json({ 
-  //       error: 'content missing' 
-  //     })
-  //   }
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'name or number missing' 
+      })
+    }
+
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+    }
   
-  //   const note = {
-  //     content: body.content,
-  //     important: Boolean(body.important) || false,
-  //     id: generateId(),
-  //   }
+    persons = persons.concat(person)
   
-  //   notes = notes.concat(note)
-  
-  //   response.json(note)
-  // })
+    response.json(person)
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
