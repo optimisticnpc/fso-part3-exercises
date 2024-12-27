214074e01dece5b50fadfd9d7d0154307581e968
optimisticnpc <joshuajltan@gmail.com>
2024-12-28 11:11:28 +1300

    Finished 3-8

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('postData', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ' '
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
 let persons = [
     { 
