ba28a933a1fabc05ed487f7a33c7b7b1dc9ab723
optimisticnpc <joshuajltan@gmail.com>
2024-12-28 10:31:27 +1300

    Finished 3-4

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ let persons = [
     }
   })
 
+  // It works!
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+  })
+
   // const generateId = () => {
   //   const maxId = notes.length > 0
   //     ? Math.max(...notes.map(n => Number(n.id)))
